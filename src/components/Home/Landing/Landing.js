6156e838c44d2814a4051c4250b6a618fd84b968
Manuel-Argueta <manuel.argueta0314@gmail.com>
2022-03-03 20:02:48 -0800

    began transition to 8-bit style

--- src/components/Home/Landing/Landing.js
+++ src/components/Home/Landing/Landing.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Landing.css'
-import earthImage from '../../../assets/Earth.png'
+import gameLogo from '../../../assets/GameLogo.png'
 
 function Landing() {
     return (
         <div id = "container">
             <div id = "subContainer">
-                <h1 id = "title">Planet Planter</h1>
+                <img src = {gameLogo}/>
                 <h3 id = "landingContent">An interactive blockchain clicker game to solve your boredom and save the world!</h3>
                 <button id = "playNowButton">Play Now</button> 
                 <button id = "learnMoreButton">Learn More</button> 
